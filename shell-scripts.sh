0fc83d5fab94917a1356a85c857aa1c5043a814b
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-01-20 00:02:08 +0700

    Update shell-scripts.sh

--- shell-scripts.sh
+++ shell-scripts.sh
@@ -1,3 +1,6 @@
-// remove files that are listed in the .gitignore but still on the repository
-// https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor 
+#!/bin/sh
+
+# remove files that are listed in the .gitignore but still on the repository
+# https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor 
 git rm --cached `git ls-files -i -c --exclude-from=.gitignore`
+
