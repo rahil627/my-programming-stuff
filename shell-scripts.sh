42518216c90924519eab2e55cfe442e852fba261
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-02-06 23:50:30 +0700

    Update shell-scripts.sh

--- shell-scripts.sh
+++ shell-scripts.sh
@@ -4,6 +4,8 @@
 # https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor 
 git rm --cached `git ls-files -i -c --exclude-from=.gitignore`
 
+
+
 # replace file extensions in folder
 # https://unix.stackexchange.com/questions/78821/how-can-i-rename-all-files-with-one-extension-to-a-different-extension-recursive#78822
 find some_folder -type f -name "*.bub" | 
@@ -17,3 +19,14 @@ $ extmv () {
     sed "s/\.${2}$//" | 
     xargs -I% mv -iv "%.${2}" "%.${3}"
 }
+
+
+
+# Remove the submodule entry from .git/config
+git submodule deinit -f path/to/submodule
+
+# Remove the submodule directory from the superproject's .git/modules directory
+rm -rf .git/modules/path/to/submodule
+
+# Remove the entry in .gitmodules and remove the submodule directory located at path/to/submodule
+git rm -f path/to/submodule
