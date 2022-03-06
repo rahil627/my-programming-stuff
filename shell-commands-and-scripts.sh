eab75b7a9ac919dadb57fa7e5c20a32ea6a99bb6
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-03-06 19:55:04 +0700

    Update and rename shell-scripts.sh to shell-commands-and-scripts.sh

--- shell-commands-and-scripts.sh
+++ shell-commands-and-scripts.sh
@@ -40,13 +40,6 @@ swapon -s
 # NOTHING is standardized!!! ahhhhhh
 cp init.vim ~/.config/nvim/init.vim
 
-
-# remove files that are listed in the .gitignore but still on the repository
-# https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor 
-git rm --cached `git ls-files -i -c --exclude-from=.gitignore`
-
-
-
 # replace file extensions in folder
 # https://unix.stackexchange.com/questions/78821/how-can-i-rename-all-files-with-one-extension-to-a-different-extension-recursive#78822
 find some_folder -type f -name "*.bub" | 
