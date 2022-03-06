a67d7692ebe6a81682b04efa7025430b8b8e63a1
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-03-06 19:55:35 +0700

    Update git-shell-commands.sh

--- git-shell-commands.sh
+++ git-shell-commands.sh
@@ -1,1 +1,4 @@
 
+# remove files that are listed in the .gitignore but still on the repository
+# https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor 
+git rm --cached `git ls-files -i -c --exclude-from=.gitignore`
