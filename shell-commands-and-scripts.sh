8891db07a67be371a608666246cde30db55d4444
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-03-06 19:57:14 +0700

    Update shell-commands-and-scripts.sh

--- shell-commands-and-scripts.sh
+++ shell-commands-and-scripts.sh
@@ -9,7 +9,6 @@ which zsh
 chsh
 
 
-
 # append a folder-path to the $PATH environment var
 # so that programs ("binaries") in it can be invoked via command line
 echo $PATH
@@ -54,13 +53,3 @@ $ extmv () {
     xargs -I% mv -iv "%.${2}" "%.${3}"
 }
 
-
-
-# Remove the submodule entry from .git/config
-git submodule deinit -f path/to/submodule
-
-# Remove the submodule directory from the superproject's .git/modules directory
-rm -rf .git/modules/path/to/submodule
-
-# Remove the entry in .gitmodules and remove the submodule directory located at path/to/submodule
-git rm -f path/to/submodule
