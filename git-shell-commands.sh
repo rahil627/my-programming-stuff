40387daba74e192daae3d1649c4f1688e02a0ac8
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-03-06 19:57:39 +0700

    Update git-shell-commands.sh

--- git-shell-commands.sh
+++ git-shell-commands.sh
@@ -2,3 +2,13 @@
 # remove files that are listed in the .gitignore but still on the repository
 # https://stackoverflow.com/questions/13541615/how-to-remove-files-that-are-listed-in-the-gitignore-but-still-on-the-repositor 
 git rm --cached `git ls-files -i -c --exclude-from=.gitignore`
+
+
+# Remove the submodule entry from .git/config
+git submodule deinit -f path/to/submodule
+
+# Remove the submodule directory from the superproject's .git/modules directory
+rm -rf .git/modules/path/to/submodule
+
+# Remove the entry in .gitmodules and remove the submodule directory located at path/to/submodule
+git rm -f path/to/submodule
