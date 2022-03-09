ca35c4a15537d0ccdbb28ac5328897df18eb86f5
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-03-10 04:24:16 +0700

    Update shell-commands-and-scripts.sh

--- shell-commands-and-scripts.sh
+++ shell-commands-and-scripts.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# change file encoding
+
+# get encoding of the file
+file -i file-path
+
+# encode files in the current directory
+# #!/bin/bash
+
+FROM_ENCODING="value_here" # set this
+TO_ENCODING="UTF-8//TRANSLIT" # UTF-8 is the standard encoding, //TRANSLIT tries to transliterate to a similar character, otherwise can remove it
+
+CONVERT=" iconv  -f   $FROM_ENCODING  -t   $TO_ENCODING"
+
+for  file  in  *.txt; do
+     $CONVERT   "$file"   -o  "${file%.txt}-utf8" # safer than overwriting
+done
+exit 0
+
 
 # get file-path of shell
 which zsh
