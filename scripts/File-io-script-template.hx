92c02e76f3b89b5cc71b2672afcc94213d9e2f9b
rahil627 <1132053+rahil627@users.noreply.github.com>
2022-03-10 19:41:49 +0000

    another template! yay! programming is so... modular! stuff just keeps building on top of each other !!

---
+++ scripts/File-io-script-template.hx
@@ -0,0 +1,25 @@
+
+class File-io-scipt-template {
+
+	static function main() {
+
+		// check if FileSystem is available
+		#if sys
+		trace("file system can be accessed");
+		#end
+		// else it traces "You cannot access the sys package while targeting js (for sys.FileSystem)".
+
+
+		var file_path:String = Sys.args()[0]; // just gets the first argument
+
+		// read
+		var c:String = sys.io.File.getContent(file_path);
+		trace(c);
+
+		// do stuff
+		
+
+		// write
+		sys.io.File.saveContent('file-path',c);
+	}
+}
