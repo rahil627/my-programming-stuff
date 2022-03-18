755b5c06d8ef3c46974e3863b15bf5ee252cbdae
rathewolf <1132053+rahil627@users.noreply.github.com>
2022-03-19 01:04:14 +0700

    Update git-shell-commands.sh

--- git-shell-commands.sh
+++ git-shell-commands.sh
@@ -38,8 +38,7 @@ git rm --cached `git ls-files -i -c --exclude-from=.gitignore`
 
 # add a git submodule
 git submodule add https://bitbucket.org/jaredw/awesomelibrary
-
-git add .gitmodules awesomelibrary/
+# it should add .gitmodules and a folder for it
 
 
 # remove a git submodule
