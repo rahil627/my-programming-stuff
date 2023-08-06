b4a80c5833685cc3e4dc82ad0a113a26bc0cc92c
rahil627 <1132053+rahil627@users.noreply.github.com>
2023-08-06 03:42:39 -0700

    ruby 101

--- scripts/parse-resume.rb
+++ scripts/parse-resume.rb
@@ -1,11 +1,73 @@
 #!/usr/bin/ruby
 
-# https://www.tutorialspoint.com/ruby/ruby_input_output.htm
-aFile = File.new("input.txt", "r")
-if aFile
-   content = aFile.sysread(20)
-   puts content
-else
-   puts "Unable to open file!"
+filename = "Gemfile"
+tags = ["hospitality", "tech", "community"]
+
+# console part
+puts tags
+puts "enter one of the above"
+input = gets.chomp # very important to "chonp" newlines!
+
+puts input
+if (not(tags.include?(input)))
+   throw "input was wrong"
 end
 
+
+# https://www.tutorialspoint.com/ruby/ruby_input_output.htm
+#f = File.read(filename)
+#puts f
+#f.chomp
+
+#IO.foreach(filename) do |line|
+#   if line.
+#   puts line if (line[/ohn\b/])
+#end
+
+#positions_at_beginning_of_paragraphs = []
+#positions_at_tags
+
+position_at_beginning_of_paragraph = 0
+output = ""
+concatenating = false;
+#c = 0
+
+File.open('dummy.txt') do |f|
+   f.each_line do |line|
+      if concatenating = true
+         output += line
+
+         if line.include?("####") && (f.pos != position_at_beginning_of_paragraph)
+            # until the second #### line
+            #c++
+            
+            #if c == 2
+               concatenating = false
+              # c = 0
+               break
+            end
+         end
+      end
+
+      if line.include?("####")
+         # f.rewind # rewind to beginning of line? or file?
+         position_at_beginning_of_paragraph = f.pos
+      end
+
+      if line.include?("tags: ")
+         if line.include?(input)
+            f.pos = position_at_beginning_of_paragraph
+            concatenating = true;
+         end
+      end
+      p "position=#{f.pos} eof?=#{f.eof?} lineno=#{f.lineno}"
+      
+   end
+end
+
+
+#aFile = File.new(filename, "r")
+#if not(aFile)
+#   puts "Unable to open file!"
+
+# File.readline("tags")
