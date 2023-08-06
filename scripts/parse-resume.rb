c99d5883c357226226765716a42a0d6c4ea78915
rahil627 <1132053+rahil627@users.noreply.github.com>
2023-08-06 03:55:34 -0700

    sleep

--- scripts/parse-resume.rb
+++ scripts/parse-resume.rb
@@ -9,6 +9,7 @@ puts "enter one of the above"
 input = gets.chomp # very important to "chonp" newlines!
 
 puts input
+
 if (not(tags.include?(input)))
    throw "input was wrong"
 end
@@ -27,36 +28,33 @@ end
 #positions_at_beginning_of_paragraphs = []
 #positions_at_tags
 
-position_at_beginning_of_paragraph = 0
+lineno_at_beginning_of_item = 0
 output = ""
 concatenating = false;
-#c = 0
 
 File.open('dummy.txt') do |f|
    f.each_line do |line|
+
       if concatenating = true
          output += line
 
-         if line.include?("####") && (f.pos != position_at_beginning_of_paragraph)
+         if line.include?("####") && (f.lineno != lineno_at_beginning_of_item)
             # until the second #### line
-            #c++
-            
-            #if c == 2
                concatenating = false
-              # c = 0
-               break
             end
          end
+
+         break
       end
 
       if line.include?("####")
          # f.rewind # rewind to beginning of line? or file?
-         position_at_beginning_of_paragraph = f.pos
+         lineno_at_beginning_of_item = f.lineno
       end
 
       if line.include?("tags: ")
          if line.include?(input)
-            f.pos = position_at_beginning_of_paragraph
+            f.lineno = lineno_at_beginning_of_item
             concatenating = true;
          end
       end
