f614c418ae38dcfb8958587a3ff1a70806133354
rahil627 <1132053+rahil627@users.noreply.github.com>
2023-08-11 05:02:10 -0700

    add custom fzf bindings

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -9,10 +9,28 @@ if status is-interactive
 	
 	# not sure.. maybe for reverse search?
 	#[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# remove default fzf bindings
+  bind -e \ct fzf-file-widget
+  bind -e \cr fzf-history-widget
+  bind -e \ec fzf-cd-widget
+  if bind -M insert > /dev/null 2>&1
+    bind -e -M insert \ct fzf-file-widget
+    bind -e -M insert \cr fzf-history-widget
+    bind -e -M insert \ec fzf-cd-widget
+  end
+
+# add new fzf bindings
+  bind \ef fzf-file-widget
+  bind \eh fzf-history-widget
+  bind \ec fzf-cd-widget
+  if bind -M insert > /dev/null 2>&1
+    bind -M insert \ef fzf-file-widget
+    bind -M insert \eh fzf-history-widget
+    bind -M insert \ec fzf-cd-widget
+  end
+
+
 end
-
-
-
-
 
 
