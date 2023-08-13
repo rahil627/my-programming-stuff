1709d12dfce5c62e247ddb922b66972298c40802
rahil627 <1132053+rahil627@users.noreply.github.com>
2023-08-13 00:16:50 -0700

    default to vi bindings, yay! 'n clean up fish conf

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,19 +1,31 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# not status is-interactive || exit
+if not status is-interactive
+	return 1
+end
 
-	# don't need this for fedora, just let it use the default path
-    # Install Ruby Gems to ~/gems
-    #set -gx PATH $PATH ~/bin
-    #set -gx GEM_HOME $HOME/gems
-	#set -gx PATH $PATH $HOME/gems/bin
-	
-	# not sure.. maybe for reverse search?
-	#[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+# Commands to run in the startup of interactive sessions can go here
+function bind-keys
+  # use vi key bindings instead of the default emacs key bindings
+  # note: this ERASES ALL PREVIOUS BINDINGS by default!
+  fish_vi_key_bindings
 
-# remove default fzf bindings
+  # optional: can combine the default/emacs bindings
+  # Execute this once per mode that emacs bindings should be used in
+  #fish_default_key_bindings -M insert
+
+  # Then execute the vi-bindings so they take precedence when there's a conflict.
+  # Without --no-erase fish_vi_key_bindings will default to
+  # resetting all bindings.
+  # The argument specifies the initial mode (insert, "default" or visual).
+  #fish_vi_key_bindings --no-erase insert
+
+
+  # remove default fzf bindings (see .fzf/shell/key-bindings.fish)
+  # not sure where it's getting called... .fzf.fish plugin?
   bind -e \ct fzf-file-widget
   bind -e \cr fzf-history-widget
   bind -e \ec fzf-cd-widget
+  # for insert mode of vim mode
   if bind -M insert > /dev/null 2>&1
     bind -e -M insert \ct fzf-file-widget
     bind -e -M insert \cr fzf-history-widget
@@ -29,8 +41,14 @@ if status is-interactive
     bind -M insert \eh fzf-history-widget
     bind -M insert \ec fzf-cd-widget
   end
-
-
 end
+
+
+bind-keys
+
+# not sure.. maybe for reverse search?
+#[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+
 
 
---
+++ play/herro.cr
@@ -0,0 +1,10 @@
+v = "herro"
+p! v, typeof(v)
+
+v.downcase
+
+"test".upcase
+
+# TODO:
+# autocomplete is slow, i think it's building every-time it checks for auto-complete
+
