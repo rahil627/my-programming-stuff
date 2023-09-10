533b3aa6ebdd0ffd2d820b87d3a4e02770361cdf
rahil627 <1132053+rahil627@users.noreply.github.com>
2023-09-10 02:14:38 -0700

    linux ruby hell-hole

--- langs/setup-ruby.sh
+++ langs/setup-ruby.sh
@@ -44,25 +44,49 @@ ruby-install ruby
 # make sure everthing is ok
 gem env
 
+# https://wiki.archlinux.org/title/ruby#Configuration
+# By default in Arch Linux, when running gem, gems are installed per-user (into ~/.local/share/gem/ruby/), instead of system-wide (into /usr/lib/ruby/gems/)
+# The recommended way to setup that is by manually specifying your $GEM_HOME, which then can be appended to your $PATH environment variable in order to allow RubyGems binaries to be executed:
+
+# set everything to use a single gem dir, the user dir not the system/default dir 
+# https://felipec.wordpress.com/2022/08/25/fixing-ruby-gems-installation/
+# setting $GEM_HOME overrides gem.paths.home
+# bundle doesn't know where to install gems otherwise
+#set -gx GEM_HOME "$(gem env user_gemhome)" # this seems to work fine too...
+#set -gx GEM_HOME $(ruby -e 'puts Gem.user_dir')
+echo -e "\nset -gx GEM_HOME $(ruby -e 'puts Gem.user_dir')"
+# /home/ra/.gem/ruby/3.0.0
+# in archlinux, it uses ~/.local/share/gem/ruby/3.0.0
+# if there's anything installed there, move gems to the ~/.gem/ruby/3.0.0 path and then delete it
+# i think chruby uses ~/.gem dir
+
 # add gem path to $path
 # NOTE: this might clog up shell commands namespace...
 # use user gem path over root?
-fish_add_path /home/ra/.local/share/gem/ruby/3.0.0/bin
+fish_add_path $GEM_HOME/bin
+# do i add the root gem dir too? (gem env gemhome or ruby -e Gem.default_dir)
+
+
 
 
 
 # add lsp
-# the built-in lsp will use this gem's binaries (rct-complete, etc.)
-gem install rcodetools
+# shopify's lsp was released in 2021, after solargraph
+# it doesn't require building yard docs
+
+# the built-in(?) lsp will use this gem's binaries (rct-complete, rct-doc, etc.)
+# although the completion worked, it didn't show doc previews, and the completion wasn't ordered by class inheritance, making it pretty useless
+#gem install rcodetools
 
 # a contemporary, more featureful ruby lsp
 # it adds previews on hover, documentation of classes via side-bar
 # it also seems to order class methods by inheritance, which is crucial in ruby's hella deep objects
-gem install solargraph 
+#gem install solargraph 
 # documentation gem used by solargraph, super-set of original RDoc
-gem install yard 
+#gem install yard 
 # generate docs for all installed gems (to ~/.yard)
-yard gems
+#yard gems
+# (NOTE: for each project, have to add solargraph to Gemfile, and run 'yard docs')
 
 
 
