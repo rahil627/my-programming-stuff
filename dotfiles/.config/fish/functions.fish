e51933989acda4fc4224e4f7a36599ef3aba16f6
rahil627 <1132053+rahil627@users.noreply.github.com>
2023-09-15 01:42:47 -0700

    eXecute a command in a Directory....

--- dotfiles/.config/fish/functions.fish
+++ dotfiles/.config/fish/functions.fish
@@ -35,3 +35,27 @@ end
 funcsave lfcd
 
 
+
+# https://github.com/kuncevic/execute-dir.fish/blob/master/functions/xd.fish
+# execute a command in a different working directory
+function xd
+    if test (count $argv) -lt 2
+        echo "Usage: xd <dir> <command>"
+        return 1
+    end
+
+    set dir $argv[1]
+    set cmd $argv[2..-1]
+
+    if test -d $dir
+        pushd $dir > /dev/null
+        $cmd
+        popd > /dev/null
+    else
+        echo "Directory '$dir' does not exist."
+        return 1
+    end
+end
+funcsave xd
+
+
