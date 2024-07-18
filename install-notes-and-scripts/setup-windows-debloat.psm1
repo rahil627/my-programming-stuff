fa1818b5b79047d0ebc420a363ebf2eb19dca1e0
ra <1132053+rahil627@users.noreply.github.com>
2024-07-17 22:51:02 -0700

    minor bug

--- install-notes-and-scripts/setup-windows-debloat.psm1
+++ install-notes-and-scripts/setup-windows-debloat.psm1
@@ -1,14 +1,14 @@
 # WARNING: be careful when removing ("tweaking") things from windows, often, it's not worth the damage it can do. Instead, look for ways to simply turn things off.
 # if you do remove the wrong package, use the reinstall-removed-package.ps1 script to add it back
 
-$apps_to_remove @{
-    app = "app-search"
+$apps_to_remove = @{
+    app = "app-id"
     #widget = "windows web experience pack" # not an id. i think it searches
     # just turn this off in settings, doh!
 }
 
-function uninstall_app($app) {
-    winget uninstall $app # hash value .tostring?
+function uninstall_app($id) {
+    winget uninstall --id $id # hash value .tostring?
 }
 
 function remove_all_bloatware($apps) {
