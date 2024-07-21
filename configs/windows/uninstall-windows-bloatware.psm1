65002b5a2d958aadc91f4ad0997a416ff4cb3a0d
ra <1132053+rahil627@users.noreply.github.com>
2024-07-20 23:13:51 -0700

    remove a bit more crap

--- configs/windows/uninstall-windows-bloatware.psm1
+++ configs/windows/uninstall-windows-bloatware.psm1
@@ -7,12 +7,17 @@ $bloatware = @{
     #  - to find the app's id/name
 
     # to try:
+    # media player (vs vlc media player)
+    # news
+    # sound recorder
     # clipchamp
     # xbox
 
     # ok:
     # paint # still the best ms native app
     # notepad
+    # maps # this is actually ok!
+
     # calculator
     # snipping tool (though it should be a powertoy)
 
@@ -23,13 +28,14 @@ $bloatware = @{
     #edge # can't uninstall (neatly) :(
     outlook = "outlook (new)" # TODO: is it worth even trying..??
     ms_games = "solitaire & casual games" # takes 114mb and doesn't even have the games installed..??
+    movies = "movies & tv" # just plain terrible cash-grab
     xbox = "xbox" # TODO: free ultimate pass for 1 month, then trash it
 
+    email = "mail and calendar" # read it has ads, $$
     todo = "todo" # use simple text file
     clock = "Windows Clock" # use phone, [9WZDNCRFJ3PR], # TODO: is this a permanent id or does it change by version?
     sticky_notes = "Microsoft Sticky Notes" # [9NBLGGH4QGHW]
     # uses sqllite! wtf!! otherwise, i kinda dig the mess of multiple notes all over the desktop
-
 
     #widget = "windows web experience pack" # not an id. i think it searches
     # just turn this off in settings, doh!
