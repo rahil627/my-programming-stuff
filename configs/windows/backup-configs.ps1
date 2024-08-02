1af8d76d9caf15389dc487ca3ff2993b3c8885ef
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-08-01 23:27:57 -0700

    +backup-configs place-holder

---
+++ configs/windows/backup-configs.ps1
@@ -0,0 +1,7 @@
+
+# generally, just copy from AppData
+# my god, windows is so much more simple! far far less cognitive load than linux
+
+# todo: need env var for path to repos
+# TODO: test how strings work
+# cp $home\AppData\Roaming\helix\config.toml $home\my-stuff\repos\my-programming-stuff\configs\helix\
