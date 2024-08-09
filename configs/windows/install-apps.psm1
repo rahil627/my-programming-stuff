59428cbc66c903da2c5ecae157f93c8a22f650b5
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-08-09 14:35:37 -0700

    Update install-apps.psm1

--- configs/windows/install-apps.psm1
+++ configs/windows/install-apps.psm1
@@ -8,6 +8,10 @@
 # given that i'm aiming for more privacy, alternative apps (ungoogled chromium, vscodium) will never exist on msstore
 # from a comment on reddit: "I always use winget. Store versions aren't always current. Not sure if they get auto updated, as it's not installing them via the store, just using the store's repository. I also skip the store version because you have to be signed in to use it and I use this on machines that aren't tied to an MS account. They're definitely not sandboxed."
 
+# scoop > winget?
+# TODO: when to install using scoop vs winget?
+# scoop seems great for dev stuff, as it creates aliases ("shims") for you
+
 # https://winstall.app/
 # simple site to search through winget repos
 
@@ -27,21 +31,22 @@ $apps_to_try =  @( # array
     # https://github.com/hlaueriksson/awesome-powertoys-run-plugins
     #  - everything plugin for powertoys run
 
-    "SublimeHQ.SublimeText" # TODO: --include-unknown
+    "SublimeHQ.SublimeText", # TODO: --include-unknown
 
-    "SomePythonThings.WingetUIStore"
-    "GitHub.GitHubDesktop" # compare with vs-code
-    "Microsoft.PCManager"
+    "SomePythonThings.WingetUIStore",
+    "GitHub.GitHubDesktop", # love it for commit, until i try lazygit 'n magit..
+    "Microsoft.PCManager",
 
     # dendron is a vs-code extension
-    "Obsidian.Obsidian"
-    #"Logseq.Logseq"
-    #"Joplin.Joplin"
+    "Obsidian.Obsidian",
+    #"Logseq.Logseq",
+    #"Joplin.Joplin",
 
     "appmakes.Typora"
 )
 
-$apps = @{ # enum won't work, and psobject seems like a hassle
+# winget app ids
+$app_ids = @{ # hash-table; enum won't work, and psobject seems like a hassle
 
     # windows 11 pro?
     # only adds features aimed at businesses except for these important ones:
@@ -81,54 +86,137 @@ $apps = @{ # enum won't work, and psobject seems like a hassle
     # essential apps
     #simple_gui_text_editor = "SublimeHQ.SublimeText"
     terminal_text_editor = "Helix.Helix"
+
     dropbox = "Dropbox.Dropbox"
-    # todo: choose a browser
+    # TODO: clean up other cloud storages, use as backup for dropbox
+    google_drive = "Google.GoogleDrive"
+    # apple
+    # microsoft?
+
     browser = "eloston.ungoogled-chromium" # with uBlock
     # setup_browser
+    # other options to try:
     # brave # comes with adblocker, privacy, etc.
     # cromite
     # librewolf or betterfox # firefox with sane defaults
     spotify = "Spotify.Spotify"
     discord = "Discord.Discord" #"SlackTechnologies.Slack"
-    #Telegram.TelegramDesktop # just use phone app
-    #Microsoft.Skype
+    # signal = "OpenWhisperSystems.Signal" # just use phone app
+    # facebook-messenger
 
     steam = "Valve.Steam"
 
-    # programming-related
+    # TODO: to automation.txt
+    # https://www.reddit.com/r/ifttt/comments/ixy5ke/ifttt_alternatives_if_you_cant_afford_to_pay/
+    # find some IFTTT-like work-flow cloud service automation software
+    # power automate eating up battery, 40 minutes!
+    # it also simply doesnut work: error upon startup
+    # also has a steep learning curve
+    # automation = "power automate"
+    # try n8n instead, itus free, open-source, can self-host, has hosted version
+    # nvm, cheapest plan is $20/month! must self-host
+    # seems quite complicated compared to IFTTT :/
+    # for now, try rclone to sync cloud storages
+
+    # shell
+    # (install cli-apps with scoop)
     powershell = "Microsoft.Powershell" # strangely, windows 11 ships with v1, currently at v7!!
     # make it the default terminal
     #   - open terminal -> click on the down-arrow near new tab location -> set startup/default profile
     # todo: no way to do this via command line? :/
-    #vscode = "Microsoft.VisualStudioCode"
+
+    # programming-related
+    #vscode = "Microsoft.VisualStudioCode",
     vscode = "VSCodium.VSCodium" # works the same!, just extensions rely on people  adding them to a list
-    git = "Git.Git" #winget install --id Git.Git -e --source winget # TODO: what's the -e flag?
-    lazygit = "JesseDuffield.lazygit" # TODO vs magit
+    git = "Git.Git" #winget install --id Git.Git -e --source winget # TODO: whatjs the -e flag?
+    lazygit = "JesseDuffield.lazygit" # TODO vs tig vs magit
+    # tig comes installed with 'git for windows' bundle
+    #  - it also includes "git credential manager"
 
     # more dev specific
-    # erlang = "Erlang.ErlangOTP" # for elixir
-    # elixir
-    #  - used scoop
     sql = "postgresql" # TODO: add install by search, add --source winget
-    # "PostgreSQL.PostgreSQL 16"
+    # "PostgreSQL.PostgreSQL 16",
 
     #tightnvc?
 
- } #| foreach {winget install -e --id $_}
+}  #| foreach {winget install -e --id $_}
 
 
-# used by scoop
+# used by scoop, but can work for winget too
 $app_names = @(
 
+# cli-apps (the best of linux, on windows! :) )
+# aim for contemporary binary replacements (rust/go/zig/etc.)
+
+# copied from setup-linux.sh:
+# TODO: try installing the ones i had on linux
+# core shell apps
+
+# fzf
+# holy shit, search entire OS with this!
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
+# note: i have this exact two lines included in the nvim plug-in manager too
+# prompts:
+# autocomplete? yes
+# bindings? TODO
+# shell integration? nah (adds to .bashrc and some fish config file
+
+# file directory navigator
+# TODO: try them all!
+    "yazi", # vs lf (my previous app) vs nnn vs mc (classic)
+    #  - looks great upon first try!
+    # broot # a different way
+
+# replacement basic commands
+# TODO: not sure if these work for powershell
+    "eza", # ls; try lsd
+    "bat", # cat
+
+# replacement utilities
+# https://news.ycombinator.com/item?id=26561211
+# https://wiki.archlinux.org/title/core_utilities#Alternatives
+  # - great list
+    "ripgrep",
+# sudo pacman -S ripgrep # grep & ack
+# TODO: try fzf+ripgrep
+# https://github.com/junegunn/fzf#3-interactive-ripgrep-integration
+    "fd",
+# sudo pacman -S fd # find & parallel, fd-find package in debian
+# zoxide > cd
+    "delta", # diff
+# sudo pacman -S delta # diff
+# xh > curl & httpie
+# sudo pacman -S sd # sed
+
+# dust or ncdu > du
+    "dust", # ncdu > du; great for finding what's taking up disk space
+
+# procs > ps
+# bottom > top
+
+
+
+# essentials
+# simple community-sourced help for most commands, super practical
+    "tldr",
+# sudo pacman -S tldr # man
+# one time, it didn't update the dictionary upon install, so...
+# tldr -u
+
+
     # dev
-    "erlang",
-    "elixir",
-    "haxe"
+    "erlang", # dependency for elixir
+    "elixir", # web/phoenix
+    "haxe" # general; super easy to setup compared to ruby!
+    # "ruby", # scripting, libs
+    # go # ? # simpler alt to haxe
+    # MSYS2 vs WSL
 )
 
-# example use:
-# install_app($apps.shell)
-function install_app($id) { # restriction: '-' is a special char for powershell commands
+# use: install_app_by_id($app_ids.shell)
+# NOTE: uses winget
+function install_app_by_id($id) { # restriction: '-' is a special char for powershell commands
     $output = winget install --id $id --source winget # store string to avoid command substitution
     # without id, i think it will search by name by default and run into multiple choices/sources
     # with --source winget, can consistently upgrade all apps via "winget --upgrade" instead of the windows store
@@ -145,13 +233,16 @@ function install_app($id) { # restriction: '-' is a special char for powershell 
 
 }
 
+# use: install_app_by_name($app_names.haxe)
+# NOTE: uses scoop at the moment
 function install_app_by_name($name) { # no function parameter overloading
     scoop install $name
 }
 
-function install_apps($apps, $app_names) { # todo: is this casting a dynamic var?
-    foreach ($app in $apps) {
-        install_app($app); # app.ToString() by default
+# main script
+function install_all_apps($app_ids, $app_names) { # todo: is this casting a dynamic var?
+    foreach ($app in $app_ids) {
+        install_app_by_id($app); # app.ToString() by default
     }
 
     foreach ($app in $app_names) {
@@ -159,7 +250,3 @@ function install_apps($apps, $app_names) { # todo: is this casting a dynamic var
     }
 }
 
-# main script
-function install_all_apps() {
-    install_apps($apps, $app_names)
-}
