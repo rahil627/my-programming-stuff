0f80477f9c7a905d2a15f5c451be33b67b0ad771
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-09-16 01:58:58 -0700

    unimplemented for now

--- configs/windows/load-configs.ps1
+++ configs/windows/load-configs.ps1
@@ -1,5 +1,20 @@
-# tempfix
-cp .\powershell\Microsoft.PowerShell_profile.ps1 $home\OneDrive\Documents\PowerShell\
+
+# because i'm in the process of changing the tools i use (terminal tools vs emacs), just leave this unimplemented for now
 
 # these configs are generated, so must load
-cp "$my_configs\terminal\settings.json" $config_paths['terminal']
+# NOTE: avoid changing settings via gui, edit settings in the config repo, and then load them all
+# cp "$my_configs\terminal\settings.json" $config_paths['terminal']
+#  - now use wezterm
+
+
+
+# NOTE: should only have to run this script once, upon os install. normally, you'd just copy a single config file at a time
+write-warning "unimplemented"
+# see configs folder and path vars set in powershell profile
+# cp .\powershell\Microsoft.PowerShell_profile.ps1 $home\OneDrive\Documents\PowerShell\
+
+
+# TODO: could create a function to load all configs, and another to load a single config
+# in powershell profile, create a list of os paths correlating to each config file in the repo
+# then, here, it'll be:
+# cp $config_paths_local['app'] $config_paths_windows['app']
