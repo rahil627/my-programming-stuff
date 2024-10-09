62b07aa5bf49bba74b80e06b7376e25002b633d1
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-10-09 07:44:47 -0700

    update scoop apps

--- configs/windows/install-apps.psm1
+++ configs/windows/install-apps.psm1
@@ -117,23 +117,24 @@ $apps = @{ # hash-table; enum won't work, and psobject seems like a hassle
     # seems quite complicated compared to IFTTT :/
     # for now, try rclone to sync cloud storages
 
-    # shell
-    # (install cli-apps with scoop)
+
+    # dev
+    # NOTE: use scoop to install all cli-apps
+    #       also try to use scoop to install dev-related gui tools: wezterm, emacs, etc.
+
     powershell = "Microsoft.Powershell" # strangely, windows 11 ships with v1, currently at v7!!
     # make it the default terminal
     #   - open terminal -> click on the down-arrow near new tab location -> set startup/default profile
     # todo: no way to do this via command line? :/
 
-    # programming-related
     #vscode = "Microsoft.VisualStudioCode",
     vscode = "VSCodium.VSCodium" # works the same!, just extensions rely on people  adding them to a list
-    git = "Git.Git" #winget install --id Git.Git -e --source winget # TODO: whatjs the -e flag?
+    # git = "Git.Git" #winget install --id Git.Git -e --source winget # TODO: whatjs the -e flag? # TEMP: try scoop instead
     github = "GitHub.GitHubDesktop" # love it for commit, until i try lazygit 'n magit..
     lazygit = "JesseDuffield.lazygit" # TODO vs tig vs magit
     # tig comes installed with 'git for windows' bundle
     #  - it also includes "git credential manager"
 
-    # more dev specific
     sql = "postgresql" # TODO: add install by search, add --source winget
     # "PostgreSQL.PostgreSQL 16",
 
@@ -145,20 +146,102 @@ $apps = @{ # hash-table; enum won't work, and psobject seems like a hassle
 # used by scoop, but can work for winget too
 $app_names = @(
 
+    # NOTE: normally scoop is used for non-gui apps, thus, for apps that don't follow the guidelines of scoops apps, such as non-gui apps, other non-official databases (buckets) must be added manually by the user
+    # scoop bucket add extras # TODO: move to install scoop
+    
     # dev
+
+    # frequent dependencies
+    "ripgrep"
+    "fd"
+    #  - these two are needed by: doomemacs, yazi
+    "git"
+
+    # terminals, multi-plexors
+    # windows terminal is pre-installed TODO: but maybe can re-install it using winget, so that it updates with winget..??
+
+    # "warp" # TODO: windows is on wait-list, can try via WSL
+
+    "extras/wezterm", # NOTE: currently triggers as a false-positive TODO: turning off the anti-virus still didn't do it.. :/ winget had the same problem. manually installed the nightly build
+    #  - https://wezfurlong.org/wezterm/installation
+
+    # "extras/alacritty",
+    #   - maybe not a good replacement for windows terminal, as it does not handle window pane splitting. It is, however, great in conjunction with zellij:
+    
+    # "zellij",
+    #  - no windows binary at the moment, as it uses some non-cross-platform libraries. can use it in WSL though!
+    #    - https://github.com/zellij-org/zellij/issues/316
+    #  - definitely better than windows terminal
+    #    - https://zellij.dev/screencasts/
+    #  - awesome-zellij has some plugins
+    #    - https://github.com/zellij-org/awesome-zellij?tab=readme-ov-file
+    #    - zellij-forgot, to show a searchable pop-up list of commands (very doom/helix-esque!)
+    #    - zellij-jump-list, emacs-like
+
+
+    # shells
+    # used winget for powershell, as it's maintained by microsoft
+
+    # text editors
+    "focus-editor" # NOTE: it's a gui-app
+    "helix",
+    # 'helix' suggests installing 'extras/vcredist2022'
+    # BUG: TODO: initially installed using winget, now unable to find Helix.Helix package in 'winget list', yet it still exists and used in $PATH for hx: C:\Users\ra\AppData\Local\Microsoft\WinGet\Packages
+    "emacs"
+    # NOTE: may be better to download compiled emacs
+    # must go through instructions to install doom-emacs
+    # NOTE: doom emacs requries ripgrep and fd
+
+    # langs    
+    "ruby", # scripting, libs; my second <3 # NOTE: not needed for dragonruby
+    "haxe", # general; super easy to setup compared to ruby! my first <3 (after actionscript)
+      # might ask to install neko
+    "sbcl", # TODO: to try
+    # smalltalk # TODO: worth trying
     "erlang", # dependency for elixir
     "elixir", # web/phoenix
-    "haxe" # general; super easy to setup compared to ruby!
-    # "ruby", # scripting, libs
-    # go # ? # simpler alt to haxe
-    # MSYS2 vs WSL
+    #  - MSYS2 vs WSL # required for ruby??
+    #    - msys2 is just 50mb, and the msys2 command runs a bash shell!
+    #    - wsl distros are <200mb but maybe decompresses to <1gb
+    
+    # wren # luxe
+    #   - currently just needs luxe vs-code extension and this version of msvc++ runtime:
+    # winget install Microsoft.VCRedist.2013.x86
+    
+    # phoenix
+    "postgres"
+
+
+
+    # build tools
+
+    # rust -> windows target via C/C++ via msvc
+    # scoop install rustup-msvc # TODO: rustup was recommended over rust-msvc by scoop NOTE: only need one installation of either rust-msvc or rustup-msvc
+    # winget install Microsoft.VisualStudio.2022.BuildTools # NOTE: it should download and automatically run. it's just an installer. can delete after done installed, if wanted
+      # i ended up manually downloading and using the visual studio installer to install everything
+    # TODO: get notes from browser tabs
 
 
 
     # cli-apps (the best of linux, on windows! :) )
     # aim for contemporary binary replacements (rust/go/zig/etc.)
 
+    # file directory navigator
+    # TODO: try them all!
+        "yazi", # (rust) vs lf (my previous app; written in go) vs nnn vs mc (classic)
+        #  yazi looks and feels great upon first try!
+        # broot # a different way TODO: worth trying some more
+
+    # fuzzy-search
+    # TODO: install fzf
+
+    # replacements for core/pre-installed cli tools
+    "zoxide" # <- cd # TODO: still trying, not sure if this messes up 'cd -/+', otherwise, why not replace the cd command with it (without replacing it when running scripts)?
+    
+
+
     # copied from setup-linux.sh:
+    # NOTE: i haven't installed these, except yaz
     # TODO: try installing the ones i had on linux
     # core shell apps
 
@@ -178,12 +261,6 @@ $app_names = @(
     # autocomplete? yes
     # bindings? TODO
     # shell integration? nah (adds to .bashrc and some fish config file
-
-    # file directory navigator
-    # TODO: try them all!
-        "yazi", # vs lf (my previous app) vs nnn vs mc (classic)
-        #  - looks great upon first try!
-        # broot # a different way
 
     # replacement basic commands
     # TODO: not sure if these are better than powershell's default ls/bat, aren't object-oriented/attribute-formatted
