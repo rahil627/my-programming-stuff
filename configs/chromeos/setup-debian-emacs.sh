458625e37791e0ebc9b88ea009da52cdef142218
rahil627 <1132053+rahil627@users.noreply.github.com>
2024-11-09 00:10:05 -0800

    stupid simple chromeos emacs setup

---
+++ configs/chromeos/setup-debian-emacs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# just a script to install emacs
+# fuck the rest of the terminal crap, emacs is all i need
+
+# just install github desktop for now..
+# downlaoad/wget from github.com/shiftkey/desktop/releases
+# - NOTE: CAUTION: 500mb!! maybe better to learn magit..
+# download and then sudo apt install ./file-installer
