dc866fc49a1778974c39a1f968389300ff35221c
rahil627 <1132053+rahil627@users.noreply.github.com>
2025-11-10 17:35:36 -0800

    + ubuntu mono font

--- configs/doom emacs/config.el
+++ configs/doom emacs/config.el
@@ -860,11 +860,13 @@
 
   ;; (setq doom-font "SeriousShanns") ; BUG: showing italics version.. though nice!
 
+
+  (setq doom-font "Ubuntu Mono-11") ; d: 10
   ;; (setq doom-font "UbuntuMono-Bront-10") ; d: 10, too small (in height), though readable!
-  (setq doom-font "UbuntuMono-Bront-11") ; NEW CLASSIC, GREAT! FAR less noisy than agave!
+  ;; (setq doom-font "UbuntuMono-Bront-11") ; NEW CLASSIC, GREAT! FAR less noisy than agave!
   ;; (setq doom-font "UbuntuMono-Bront-12") ; a bit wider..
   ;; (setq doom-font "UbuntuMono-Bront-13") ; GREAT here too, though big (shows as 15?).. like mononoki: tall yet cute!
-  ;;   - TODO: download the orginal one, to get styles
+  ;;   - TODO: to use bront version, would need to combine bront for regular font, and the original for styles
   ;;   - also has a great big X icon use by centaur-cons for close tab..!
   
   ;; (setq doom-font "DejaVu Sans Mono-9") ; d: 9, GREAT! kinda cute too! very readable, VERY narrow, excellent for long code
