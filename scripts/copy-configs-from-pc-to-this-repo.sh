d64456dfa07908a1cec4aa804762e8b9c42f53e7
rahil627 <1132053+rahil627@users.noreply.github.com>
2025-12-03 17:35:37 -0800

    linux the right way: the bare minimal

---
+++ scripts/copy-configs-from-pc-to-this-repo.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cp ~/.config/focus-editor/global.focus-config ~/my-stuff/repos/my-programming-stuff/configs/focus/global.focus-config 
+cp ~/.doom.d/config.el ~/.doom.d/init.el ~/.doom.d/packages.el ~/my-stuff/repos/my-programming-stuff/configs/doom-emacs/
+
+# NOTE: just directly edit these config files on your pc, then run this script!
+#  - TODO: run once-in-a-while?
+
+
+# TEMP: i think this is a temp solution..
+#  - i'll likely just make a git repo in the home folder for linux
+#  - though.. it sounds a bit more complicated to do for windows.. :/
+#    - some are in ../AppData/.. but some aren't..
+
+
